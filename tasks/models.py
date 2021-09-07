7a897d4c7fad30efbe0529f512565170067b9ce0
n1k0din <nik726@gmail.com>
2021-09-07 16:06:04 +0200

    Add qustion and answer models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.TextField('Текст вопроса', max_length=500)
+
+    def __str__(self):
+        return self.text[:20]
+
+
+class Answer(models.Model):
+    text = models.CharField('Текст ответа', max_length=200)
+
+    question = models.ForeignKey(
+        Question,
+        related_name='question_answers',
+        verbose_name='Вопрос',
+        on_delete=models.CASCADE,
+    )
+
+    is_correct = models.BooleanField('Правильный?', default=False)
