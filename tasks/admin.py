f5467566a3a99f70fbaa76d951c7f05f1ff4d280
n1k0din <nik726@gmail.com>
2021-09-07 17:13:37 +0200

    Modify admin panel to edit correct answers inline

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from tasks.models import Question, Answer
 
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['short_text', 'is_correct', 'question_id', 'question_short_text']
+    list_editable = ['is_correct']
+
+    def short_text(self, obj):
+        return obj.text[:50]
+
+    def question_id(self, obj):
+        return obj.question.id
+
+    def question_short_text(self, obj):
+        return obj.question.text[:150]
+
+
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Answer, AnswerAdmin)
