76840163101dbb8dc7be8357c4f59992c1b6b157
n1k0din <nik726@gmail.com>
2021-09-08 10:42:08 +0200

    Add media root and url params

--- quizer/settings.py
+++ quizer/settings.py
@@ -107,3 +107,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = env('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
