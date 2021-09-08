bc5fdda6a866831972d7224d91f72758c21ab56c
n1k0din <nik726@gmail.com>
2021-09-08 11:04:30 +0200

    Add statifiles dir param

--- quizer/settings.py
+++ quizer/settings.py
@@ -106,6 +106,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_ROOT = env('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
